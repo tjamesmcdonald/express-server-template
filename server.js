fa069c3d5e797506e58657386dfef5667a7e56e8
tjamesmcdonald <tjames.mcdonald@gmail.com>
2023-12-20 15:16:15 -0500

    adds basic paths

--- server.js
+++ server.js
@@ -16,7 +16,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // ROUTES
+// html routes
+// get / the homepage
+app.get("/", (req, res) => res.sendFile(__dirname, "index.html"))
 
+// api routes
+app.get("/api/todos", (req, res) => res.json(
+    [
+        {
+            title: "Buy Milk",
+        },
+    ]))
+
+// 404 route
+app.get('*', (req, res) => res.sendFile(__dirname, "404.html"))
 
 // START SERVER
 app.listen(PORT, () => console.log(`Sever live at port ${PORT}`))
