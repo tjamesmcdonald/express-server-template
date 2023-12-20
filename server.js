c6b34292e7c0029c7f5a5ac51cb523a3c9fd17b5
tjamesmcdonald <tjames.mcdonald@gmail.com>
2023-12-20 15:02:47 -0500

    adds base server

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 console.log("This is the server");
 // DEPENDENCIES
 // express
-
+const express = require('express');
 
 // DATA
 
 
 // APP / PORT
-
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // MIDDLEWARES
 
@@ -16,3 +17,4 @@ console.log("This is the server");
 
 
 // START SERVER
+app.listen(PORT, () => console.log(`Sever live at port ${PORT}`))
